37f63ef2f4e2d9c868cab5f7d2ee9dd9e5c83dde
Amandeep Singh <amandeep.singh@sqelabs.com>
2023-03-20 16:12:23 +0530

    Notification Mangement

---
+++ src/test/java/POMNewAgencySetup/NotificationMangementUtils.java
@@ -0,0 +1,43 @@
+package POMNewAgencySetup;
+
+import java.awt.AWTException;
+import java.util.ArrayList;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+import BrowsersBase.BrowsersInvoked;
+import BrowsersBase.DataInterface;
+import CommonMethods.CRMCommonMethods;
+import CommonMethods.RandomStrings;
+import CommonMethods.WebDriverWaits;
+import POMCRM.CSPInternalUtils;
+import POMCRM.CreateCRMUserUtils;
+import POMCRM.DashboardCRMUtils;
+import POMCRM.SubmissionSettingsUtils;
+import net.bytebuddy.utility.RandomString;
+
+
+public class NotificationMangementUtils extends AgencyCreationUtils {
+    
+	
+	//public static String loggedUserName = "//div[@class='app-header__user-name']//label";
+	//public static By ActiveTogglebtn = By.xpath(activeTooglebtn);
+
+	
+	
+	
+	
+	
+	
+	// Global Variables 
+	
+	//public static String emailTemplatesLabel ;
+	
+	 
+	 
+	 
+	 
+
+}
---
+++ src/test/java/TCsNewAgencySetup/NotificationManagement.java
@@ -0,0 +1,27 @@
+package TCsNewAgencySetup;
+
+import java.awt.AWTException;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import POMCRM.CreateCRMUserUtils;
+import POMNewAgencySetup.AppPreRequisitesUtils;
+import POMNewAgencySetup.NotificationMangementUtils;
+import TestCasesCRM.Login;
+import POMNewAgencySetup.AgencyCreationUtils;
+import POMNewAgencySetup.TemplateMangementUtils;
+
+public class NotificationManagement extends NotificationMangementUtils {
+   
+	
+	
+//	@Test(priority = )
+//	public static void AgencySetup_VerifyAllLinksForCEDashboard() throws InterruptedException {
+//		extentTest = extent.startTest(" AgencySetup_VerifyAllLinksForCEDashboard ");
+//	    extentTest.setDescription(" Verify that all links under 'Cases', 'Inspections' & 'Notices' section appear, on CE Dashboard. ");
+//	AppPreRequisitesUtils.AgencySetup_VerifyAllLinksForCEDashboard();
+//		SoftAssert softAssert = new SoftAssert();
+//		softAssert.assertAll();
+//	}
+
+}
